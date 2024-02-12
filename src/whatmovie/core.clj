5bca54d6ffc38aed8859fbe0248f3b73c97db30e
Sergey Zaborovsky <git@seryiza.xyz>
2024-02-12 06:09:36 +0600

    Add http-kit and simple http server

--- src/whatmovie/core.clj
+++ src/whatmovie/core.clj
@@ -1,4 +1,14 @@
-(ns whatmovie.core)
+(ns whatmovie.core
+  (:require [org.httpkit.server :as http-server]))
 
-(defn main []
-  (println "Hello"))
+(def server* (atom nil))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello"})
+
+(defn start-app []
+  (comment (start-app))
+
+  (reset! server* (http-server/run-server app {:port 8080})))
